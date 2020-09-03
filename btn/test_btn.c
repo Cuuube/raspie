6836c59340fdff5db910abc38635eaa29a7da30f
zxod <zxod@foxmail.com>
2020-09-03 19:55:36 +0800

    fix

--- btn/test_btn.c
+++ btn/test_btn.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 #define PIN_BTN 0
+#define LOW  0
+#define HIGH 1
 
 
 int main(int argc, char const *argv[])
@@ -20,16 +22,18 @@ int main(int argc, char const *argv[])
     while(1)
     {
         delay(10);
+
+        // 以数字电路模式，读取当前针脚的高或低
         int status = digitalRead(PIN_BTN);
         // printf("Read status is %d, current status is %d !\n", status, currentStatus);
-        
 
-        if (status == INPUT && currentStatus != status)
+        // 如果针脚接收到信号有变化，则打印log
+        if (status == LOW && currentStatus != status)
         {
             printf("Btn down!\n");
             currentStatus = status;
         }
-        else if (status == OUTPUT && currentStatus != status)
+        else if (status == HIGH && currentStatus != status)
         {
             printf("Btn up!\n");
             currentStatus = status;
--- leds/rg_led.c
+++ leds/rg_led.c
@@ -5,41 +5,59 @@
 // which gives you at least the -127 to 127 range. (-128 to 127 is common)
 // unsigned char, which gives you at least the 0 to 255 range.
 #define uchar unsigned char
-#define PIN_RED 0
-#define PIN_GREEN 1
+#define PIN_RED 1   // 红色信号针脚
+#define PIN_GREEN 2 // 绿灯信号针脚
 
-void init() {
+int init() {
     // 初始化
+    if (wiringPiSetup() == -1) { // 初始化pi
+        printf("setup wiringPi failed!");
+        return -1;
+    }
+
+    // 定义pinMode是输出信号（比如小灯），还是接收信号（比如开关）
+    pinMode(PIN_RED, OUTPUT);
+    pinMode(PIN_GREEN, OUTPUT);
+
+    // 设置可接受的范围值
     softPwmCreate(PIN_RED, 0x00, 0xff);
     softPwmCreate(PIN_GREEN, 0x00, 0xff);
+    return 0;
 }
 
 void setColor(uchar redValue, uchar greenValue) {
+    // 设置电平，必须要在之前的范围值里
     softPwmWrite(PIN_RED, redValue);
     softPwmWrite(PIN_GREEN, greenValue);
 }
 
 int main(int argc, char const *argv[])
 {
+    int initResult = init();
+    if (initResult != 0)
+    {
+        return -1;
+    }
+
     printf("----- START -----\n");
 
-    setColor(0xff, 0x00);
+    setColor(0xff, 0x00); // 绿
     delay(2000);
 
-    setColor(0x00, 0xff);
+    setColor(0x00, 0xff); // 红
     delay(2000);
 
-    setColor(0xff, 0xff);
+    // setColor(0xff, 0xff); // 本元件不可以同时发两种光
+    setColor(0xff, 0x00); // 绿
     delay(2000);
 
-    setColor(0x80, 0x80);
+    setColor(0x80, 0x00); // 半绿
     delay(2000);
 
-    setColor(0x80, 0x80);
+    setColor(0x00, 0x80); // 半红
     delay(2000);
 
     printf("----- END -----\n");
+    // 越低反而颜色权重大
     return 0;
 }
-
-
