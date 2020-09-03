d884825f2f4fc6ba379e1bfa8dcb419582c97a06
zxod <zxod@foxmail.com>
2020-09-03 20:05:29 +0800

    update

--- leds/rg_led.c
+++ leds/rg_led.c
@@ -7,6 +7,7 @@
 #define uchar unsigned char
 #define PIN_RED 1   // 红色信号针脚
 #define PIN_GREEN 2 // 绿灯信号针脚
+#define COLOR_RANGE 0xff // 颜色范围
 
 int init() {
     // 初始化
@@ -20,15 +21,21 @@ int init() {
     pinMode(PIN_GREEN, OUTPUT);
 
     // 设置可接受的范围值
-    softPwmCreate(PIN_RED, 0x00, 0xff);
-    softPwmCreate(PIN_GREEN, 0x00, 0xff);
+    softPwmCreate(PIN_RED, 0x00, COLOR_RANGE);
+    softPwmCreate(PIN_GREEN, 0x00, COLOR_RANGE);
     return 0;
 }
 
 void setColor(uchar redValue, uchar greenValue) {
     // 设置电平，必须要在之前的范围值里
-    softPwmWrite(PIN_RED, redValue);
-    softPwmWrite(PIN_GREEN, greenValue);
+    if (redValue < 0 || redValue > COLOR_RANGE) {
+        return;
+    }
+    if (greenValue < 0 || greenValue > COLOR_RANGE) {
+        return;
+    }
+    softPwmWrite(PIN_RED, COLOR_RANGE - redValue);
+    softPwmWrite(PIN_GREEN, COLOR_RANGE - greenValue);
 }
 
 int main(int argc, char const *argv[])
@@ -41,22 +48,24 @@ int main(int argc, char const *argv[])
 
     printf("----- START -----\n");
 
-    setColor(0xff, 0x00); // 绿
+    setColor(0xff, 0x00); // 红
     delay(2000);
 
-    setColor(0x00, 0xff); // 红
+    setColor(0x00, 0xff); // 绿
     delay(2000);
 
-    // setColor(0xff, 0xff); // 本元件不可以同时发两种光
-    setColor(0xff, 0x00); // 绿
+    // setColor(0x00, 0x00); // 本元件不可以同时发两种光
+    setColor(0xff, 0x00); // 红
     delay(2000);
 
-    setColor(0x80, 0x00); // 半绿
+    setColor(0x00, 0x80); // 半绿
     delay(2000);
 
-    setColor(0x00, 0x80); // 半红
+    setColor(0x80, 0x00); // 半红
     delay(2000);
 
+    setColor(0xff, 0xff); // 重置
+    delay(2000);
     printf("----- END -----\n");
     // 越低反而颜色权重大
     return 0;
